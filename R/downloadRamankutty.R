e65099c6034d96e85525704101884edf20e13ab7
FeBeier <felicitas.beier@pik-potsdam.de>
2021-02-02 16:51:12 +0100

    included Ramankutty download function

---
+++ R/downloadRamankutty.R
@@ -0,0 +1,15 @@
+#' @title downloadRamankutty
+#' @description download Ramankutty available land si (Source: Ramankutty N, Foley JA, Norman J and McSweeney K (2002) The global distribution of cultivable lands: current patterns and sensitivity to possible climate change. Global Ecology and Biogeography, 11, 377-392.)
+#'
+#' @seealso  \code{\link{downloadSource}} \code{\link{readRamankutty}}
+#' @examples
+#' \dontrun{ a <- downloadSource() }
+#'
+#' @author Felicitas Beier
+#'
+
+downloadRamankutty <- function() {
+  #download.file("https://www.nelson.wisc.edu/sage/data-and-models/global-land-use/grid.php")
+
+  ### NOTE: need to specify settings for download
+}
