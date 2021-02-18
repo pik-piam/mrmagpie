0abcbcf647173910b2975fcb2da5b51b525462ae
Marcos <mppalves@gmail.com>
2021-02-18 07:44:08 +0100

    oom bug fix

--- R/calcSoilTraining.R
+++ R/calcSoilTraining.R
@@ -21,13 +21,13 @@
 
 calcSoilTraining <-
   function(lsu_levels = c(seq(0, 2, 0.2), 2.5), lpjml = "LPJmL_cgrazing", climatetype = "HadGEM2_ES:rcp8p5:co2", sar = 20) {
-    time <- magpiesets::findset("t_all")
+    # time <- magpiesets::findset("t_all")
     co2 <- c(co2="rising", noco2="static")
     GCM <- strsplit(climatetype, split = ":")
     GCM <- paste("soilc", GCM[[1]][1],GCM[[1]][2],sep = "_")
 
-    x <- calcOutput("SoilCarbon", aggregate = F, lsu_levels = lsu_levels, lpjml = lpjml, climatetype = climatetype, sar = sar )[,time,]
-    y <- calcOutput("Environment", climatetype=climatetype, sar = sar, aggregate = F)[,time,]
+    x <- calcOutput("SoilCarbon", aggregate = "cluster", lsu_levels = lsu_levels, lpjml = lpjml, climatetype = climatetype, sar = sar )
+    y <- calcOutput("Environment", climatetype=climatetype, sar = sar, aggregate = "cluster")
     w <- add_columns(y, addnm = GCM)
     w <- add_dimension(w, dim = 3.1, add = "lsu", nm = getItems(x, dim = 3))
     w[,,GCM] <- x
