81fdcd4a91eaede59d385e2b39e7245c86b7d7c9
David Chen <davidch@lt916.pik-potsdam.de>
2021-05-26 22:58:44 +0200

    update ISIMIP3bYields for LPJmL from isimip

--- R/calcISIMIP3bYields.R
+++ R/calcISIMIP3bYields.R
@@ -36,9 +36,16 @@ if (grepl("historical", subtype)){
   x <- toolCoord2Isocell(x, cells=cells)
 
   #pdssat has reverse naming and extra yields name ## These special cases need to be cleaned up
-  if (st$model=="pDSSAT"){
+  if (st$model == "pDSSAT"){
   x <- collapseNames(x)
   x <- dimOrder(x=x, perm=c(2,1))}
+
+   else if (st$model == "LPJmL"){
+    x <- collapseNames(x)
+    x <- dimOrder(x=x, perm=c(2,1))
+    getNames(x, dim=1)["soy"] <- "soybean"
+    }
+
   x[is.na(x)] <- 0
 
 
