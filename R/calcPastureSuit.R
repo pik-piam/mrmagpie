7338e512151136d0affdc012d583cf5ab39a571d
Marcos Alves <mppalves@gmail.com>
2021-06-08 08:47:47 +0200

    pasture shares and population

---
+++ R/calcPastureSuit.R
@@ -0,0 +1,65 @@
+#' @title calcPastureSuit
+#' @description Calculate glassland suitable for pasture management based on population and aridity criteria.
+#' @param subtype Select version, climate model and period.
+#' @return List of magpie object with results on cluster level
+#' @author Marcos Alves
+#' @examples
+#'
+#' \dontrun{
+#' calcOutput("PastureSuit")
+#' }
+#' @importFrom raster area rasterFromXYZ
+
+calcPastureSuit <- function(subtype = "ISIMIP3b:IPSL-CM6A-LR:1850-2100"){
+  x <- toolSplitSubtype(subtype, list(version = NULL, climatemodel = NULL, period = NULL))
+
+  # pasture drivers
+  population <- calcOutput("GridPop_new", subtype="all", cellular=TRUE,FiveYear=TRUE, harmonize_until=2015, aggregate=F)
+  precipitation <- list()
+  ssps <- c("ssp126","ssp230","ssp340","ssp460","ssp585")
+  for (ssp in ssps) {
+    #write a script that reads different SPPs scenarios
+    # subtype = paste(x$version, x$climatemodel,ssps,x$period, "pr", sep = ":")
+    subtype = "ISIMIP3b:IPSL-CM6A-LR:ssp126:1850-2100:pr"
+    precipitation[[ssp]] <- setNames(calcOutput("GCMClimate_new", subtype = subtype, aggregate = F),paste0("past_",ssp))
+  }
+  precipitation <- mbind(precipitation)
+
+  # Cell area calculation
+  landcoords <- as.data.frame(toolGetMapping("magpie_coord.rda", type = "cell"))
+  landcoords <- cbind(landcoords, rep(1, nrow(landcoords)))
+  landcoords <- raster::rasterFromXYZ(landcoords)
+  crs(landcoords) <- "+proj=longlat"
+  cell_size <- raster::area(landcoords)
+  cell_size <- cell_size * landcoords
+  cell_size <- as.magpie(cell_size)
+  cell_size <- toolOrderCells(collapseDim(addLocation(cell_size), dim = c("x", "y")))
+
+  # population density
+  pop_density <- population*1e6/cell_size
+  pop_density[is.infinite(pop_density)] <- 0
+  pop_density[is.nan(pop_density)] <- 0
+       # 5 hab km2 population threshold for managed pastures. Same from HYDE 3.2.
+  # pop_density[pop_density<5] <- 0
+  # pop_density[pop_density>=5] <- 1
+
+  # Aridity (the real aridity is measured as the ratio between evapotranspiration and precipitarion (I have complete this calculation))
+  aridity <- precipitation[,getYears(pop_density),]
+  aridity[aridity<1.3] = 0
+  aridity[aridity>=1.3] = 1
+
+  #pasture suitability check
+  pasture_suit <- aridity
+  pasture_suit[pop_density<5] <- 0
+  pasture_suit_area = pasture_suit * cell_size/1e6*100
+  pasture_suit_area <- collapseDim(pasture_suit_area, dim = 3.2)
+
+  return(list(
+    x = pasture_suit_area,
+    weight = NULL,
+    unit = "Mha",
+    description = "Area suitable for pasture management in mha",
+    isocountries = FALSE
+  ))
+}
+
---
+++ R/readGridPop_new.R
@@ -0,0 +1,67 @@
+#' @title readGridPop_new
+#' @description Reads in past and future (SSP1-5) gridded population data, from ISIMIP database, Past data is based on HYDE3.2, while future SSPs are based on projections from Jones & O'Neill 2016
+#' @return A MAgPIE object, cellular 0.5deg resolution, of population (millions)
+#' @param subtype past (1965-2005) or future (2010-2100)
+#' @author David Chen, Marcos Alves
+#' @importFrom raster brick rasterToPoints
+#' @import magclass
+#' @importFrom dplyr left_join
+#' @import magpiesets
+
+
+readGridPop_new <- function(subtype) {
+
+  mapping <- toolGetMapping(type="cell", name="CountryToCellMapping.csv")
+
+  if (subtype=="past"){
+    ini_past  <- as.numeric(gsub("y", "", findset("past")))[1]
+    past_file <- "population_histsoc_0p5min_annual_1861-2005.nc"
+    years     <- as.numeric(unlist(regmatches(past_file, gregexpr("\\d{4}", past_file))))
+    y_years   <- paste0("y", seq(years[1], years[2], 1))
+    x         <- brick(past_file)
+    names(x)  <- y_years
+    x <- subset(x,paste0("y",seq(ini_past,years[2],1)))
+    extent(x) <- c(-180,180,-90,90)
+    x         <- rasterToPoints(x)
+    colnames(x)[c(1, 2)] <- c("lon", "lat")
+    x         <- left_join(mapping, x, by = c("lat", "lon"), copy = TRUE)
+    drop      <- c("^iso$|lpj|^cell$|lon|lat")
+    x         <- as.magpie(x[, !grepl(drop, colnames(x))])
+    getCells(x) <- gsub("_", ".", getCells(x))
+    x         <- x[, ini_past:years[2], ]
+  }
+
+  if(subtype=="future"){
+
+    files <- c("population_ssp1soc_0p5deg_annual_2006-2100.nc4",
+               "population_ssp2soc_0p5deg_annual_2006-2100.nc4",
+               "population_ssp3soc_0p5deg_annual_2006-2100.nc4",
+               "population_ssp4soc_0p5deg_annual_2006-2100.nc4",
+               "population_ssp5soc_0p5deg_annual_2006-2100.nc4")
+
+    read <- function(file){
+      years     <- as.numeric(unlist(regmatches(file, gregexpr("\\d{4}", file))))
+      y_years   <- paste0("y", seq(years[1], years[2], 1))
+      x         <- brick(file)
+      names(x)  <- y_years
+      x         <- rasterToPoints(x)
+      colnames(x)[c(1, 2)] <- c("lon", "lat")
+      x         <- left_join(mapping, x, by = c("lat", "lon"), copy = TRUE)
+      drop      <- c("^iso$|lpj|^cell$|lon|lat")
+      x         <- as.magpie(x[, !grepl(drop, colnames(x))])
+      getCells(x) <- gsub("_", ".", getCells(x))
+      return(x)
+    }
+
+    x <- list()
+    ssp_naming <- NULL
+    for (file in files) {
+      ssp_naming <- append(ssp_naming,paste0("pop_", toupper(unlist(regmatches(file,gregexpr("ssp[0-9]", file))))))
+      x[[file]] <- read(file)
+    }
+    x <- mbind(x)
+    getNames(x) <- ssp_naming
+  }
+  return(x)
+}
+
