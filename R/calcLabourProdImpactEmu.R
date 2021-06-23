32fb0c7148bed5165ad9116a50e92e4dfbba4dd2
mikewin-climsci <windisch@pik-potsdam.de>
2021-06-23 16:11:21 +0200

    unit change of laborprod calc function

--- R/calcLabourProdImpactEmu.R
+++ R/calcLabourProdImpactEmu.R
@@ -3,7 +3,7 @@
 #' @description based on method of Orlov et al. 2019. Economics of Disasters and Climate Change, 3(3), 191-211.
 #' @param timestep 5-year or yearly
 #' @param cellular cellular is true
-#' @return List of magpie object of gridded (0.5) productivity productivity loss in percent (0-100)
+#' @return List of magpie object of gridded (0.5) productivity as percentage of full labour prod 1
 #' @author Michael Windisch
 #' @importFrom magclass dimSums
 
@@ -17,6 +17,7 @@ calcLabourProdImpactEmu <- function(timestep = "5year", cellular = TRUE) {
     out <- out
   }
 
+  out <- (100-out)/100
 
   crop_area_weight <- dimSums(calcOutput("Croparea", sectoral = "kcr", physical = TRUE, irrigation = FALSE,
     cellular = TRUE, cells = "magpiecell", aggregate = FALSE, years = "y1995", round = 6), dim = 3)
@@ -26,7 +27,7 @@ calcLabourProdImpactEmu <- function(timestep = "5year", cellular = TRUE) {
     x = out,
     weight = crop_area_weight,
     unit = "Percentage of total labour productivity loss",
-    description = "Labour productivity loss as percentage (0-100)",
+    description = "Labour productivity impacts as percentage of full labour prod 1",
     isocountries = FALSE))
 
 }
