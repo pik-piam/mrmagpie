d2fa2c8733a6c49aa51703edb95fa102cba8812c
Kristine Karstens <karstens@pik-potsdam.de>
2021-06-25 13:09:53 +0200

    bugfix some issues with new carbon calculatation 8

--- R/calcTopsoilCarbon_new.R
+++ R/calcTopsoilCarbon_new.R
@@ -14,19 +14,15 @@
 #' @importFrom magpiesets findset
 #' @importFrom mrcommons toolCoord2Isocell
 
-calcTopsoilCarbon_new <- function(lpjml=c(natveg="LPJmL4_for_MAgPIE_44ac93de", crop="ggcmi_phase3_nchecks_9ca735cb"),
-                                  climatetype="GSWP3-W5E5:historical", fromFlows = FALSE){
+calcTopsoilCarbon_new <- function(lpjml = c(natveg = "LPJmL4_for_MAgPIE_44ac93de",
+                                            crop = "ggcmi_phase3_nchecks_9ca735cb"),
+                                  climatetype = "GSWP3-W5E5:historical", fromFlows = FALSE) {
 
-  if(climatetype=="GSWP3-W5E5:historical"){ stage <- "smoothed"
-  } else{                                   stage <- "harmonized2020"}
+  if (climatetype == "GSWP3-W5E5:historical") stage <- "smoothed"
+  else                                        stage <- "harmonized2020"
 
-  soilc_layer_natveg <- toolCoord2Isocell(calcOutput("LPJmL_new", version=lpjml["natveg"], climatetype=climatetype,
-                                                     subtype="soilc_layer", stage=stage, aggregate=FALSE))
-
-  # Check for NAs
-  if(any(is.na(topsoilc))){
-    stop("produced NA Carbon")
-  }
+  soilc_layer_natveg <- toolCoord2Isocell(calcOutput("LPJmL_new", version = lpjml["natveg"], climatetype = climatetype,
+                                                     subtype = "soilc_layer", stage = stage, aggregate = FALSE))
 
   if (fromFlows) {
 
@@ -44,15 +40,21 @@ calcTopsoilCarbon_new <- function(lpjml=c(natveg="LPJmL4_for_MAgPIE_44ac93de", c
     soilc_layer_natveg <- .getCPoolsFromFlows(soilc_layer_natveg, flow, 1995)
   }
 
-  topsoilc           <- soilc_layer_natveg[,,1] + 1/3 * soilc_layer_natveg[,,2]
+  topsoilc           <- soilc_layer_natveg[, , 1] + 1 / 3 * soilc_layer_natveg[, , 2]
   getNames(topsoilc) <- "topsoilc"
-  weight             <- dimSums(calcOutput("LanduseInitialisation", aggregate=FALSE, cellular=TRUE, nclasses="seven",
-                                           fao_corr=TRUE, input_magpie=TRUE, years="y1995", round=6), dim=3)
+
+  # Check for NAs
+  if (any(is.na(topsoilc))) {
+    stop("produced NA Carbon")
+  }
+
+  weight <- dimSums(calcOutput("LanduseInitialisation", aggregate = FALSE, cellular = TRUE, nclasses = "seven",
+                               fao_corr = TRUE, input_magpie = TRUE, years = "y1995", round = 6), dim = 3)
 
   return(list(
-    x=topsoilc,
-    weight=weight,
-    unit="t per ha",
-    description="Topsoil carbon in tons per hectar for natural vegetation.",
-    isocountries=FALSE))
+    x            = topsoilc,
+    weight       = weight,
+    unit         = "t per ha",
+    description  = "Topsoil carbon in tons per hectar for natural vegetation.",
+    isocountries = FALSE))
 }
