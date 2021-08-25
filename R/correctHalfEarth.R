bbc73025be7cae8944e1eb02c3b551ab442339d8
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-08-25 10:41:41 +0200

    upwards compatibility fix in correctHalfEarth

--- R/correctHalfEarth.R
+++ R/correctHalfEarth.R
@@ -6,18 +6,20 @@
 #' @seealso
 #'   \code{\link{readHalfEarth}}
 #' @examples
-#'
 #' \dontrun{
-#'   readSource("HalfEarth", convert="onlycorrect")
+#' readSource("HalfEarth", convert = "onlycorrect")
 #' }
 #'
 #' @importFrom madrat toolConditionalReplace
-#' @importFrom mrcommons toolCell2isoCell
+#' @importFrom mrcommons toolCell2isoCell toolCoord2Isocell
+#' @importFrom magclass hasCoords
 
-correctHalfEarth <- function(x){
-
-  x <- toolConditionalReplace(x, conditions = c("is.na()","<0"), replaceby = 0)
-  x <- toolCell2isoCell(x)
-
+correctHalfEarth <- function(x) {
+  x <- toolConditionalReplace(x, conditions = c("is.na()", "<0"), replaceby = 0)
+  if (hasCoords(x)) {
+    x <- toolCoord2Isocell(x, fillMissing = 0)
+  } else {
+    x <- toolCell2isoCell(x)
+  }
   return(x)
 }
