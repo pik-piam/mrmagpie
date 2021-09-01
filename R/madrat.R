828371009e635b08dcca1075e11d11ae9ec2638d
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-09-01 12:19:45 +0200

    updated madrat integration file

--- R/madrat.R
+++ R/madrat.R
@@ -1,26 +1,19 @@
-#' @importFrom madrat vcat toolCodeLabels
-#' @importFrom digest digest
+#' @importFrom madrat vcat
 
-.onLoad <- function(libname, pkgname){
-  madrat::setConfig(packages=c(madrat::getConfig("packages"),pkgname), .cfgchecks=FALSE, .verbose=FALSE)
-
-  # add labels for common ctype selections
-  labels <- NULL
-  for(t in c("c","n","h")) {
-    ncells <- c(seq(10,90,10),seq(100,900,100),seq(1000,10000,1000))
-    for(n in ncells) {
-      tmp <- paste0(t,n)
-      labels[tmp] <- digest::digest(list(ctype=tmp),"md5")
-    }
-  }
-  toolCodeLabels(add=labels)
+.onAttach <- function(libname, pkgname) {
+  madrat::setConfig(packages = c(madrat::getConfig("packages"), pkgname), .cfgchecks = FALSE, .verbose = FALSE)
 }
 
-#create an own warning function which redirects calls to vcat (package internal)
-warning <- function(...) vcat(0,...)
+.onDetach <- function(libpath) {
+  pkgname <- basename(libpath)
+  madrat::setConfig(packages = setdiff(madrat::getConfig("packages"), pkgname), .cfgchecks = FALSE, .verbose = FALSE)
+}
+
+# create an own warning function which redirects calls to vcat (package internal)
+warning <- function(...) vcat(0, ...)
 
 # create a own stop function which redirects calls to stop (package internal)
-stop <- function(...) vcat(-1,...)
+stop <- function(...) vcat(-1, ...)
 
 # create an own cat function which redirects calls to cat (package internal)
-cat <- function(...) vcat(1,...)
+cat <- function(...) vcat(1, ...)
