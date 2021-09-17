47a9f5384826c6541df545b7af6239c042e61ceb
Felicitas <beier@pik-potsdam.de>
2021-09-17 17:35:51 +0200

    Adjusted calcCalibratedYields such that it works with 67k cells. Also: in-code comments included and linter warnings addressed

--- R/calcCalibratedYields.R
+++ R/calcCalibratedYields.R
@@ -1,65 +1,98 @@
 #' @title calcCalibratedYields
-#' @description This functions calibrates extracted yields from LPJmL to FAO levels on country level
+#' @description This functions calibrates extracted yields from LPJmL to
+#'              FAO country level yields
 #'
-#' @param source Defines LPJmL version for main crop inputs and isimip replacement.
-#'               For isimip choose crop model/gcm/rcp/co2 combination formatted like this: "yields:EPIC-IIASA:ukesm1-0-ll:ssp585:default:3b"
-#' @param climatetype Switch between different climate scenarios
-#' @param refYear reference year for calibration
-#' @return magpie object in cellular resolution from reference year on
-#' @author Kristine Karstens
+#' @param source      Defines LPJmL version for main crop inputs and isimip replacement.
+#'                    For isimip choose crop model/gcm/rcp/co2 combination formatted like this:
+#'                    "yields:EPIC-IIASA:ukesm1-0-ll:ssp585:default:3b"
+#' @param climatetype switch between different climate scenarios
+#' @param refYear     reference year for calibration
+#' @param cells       number of cells "magpiecell" for 59199 cells or
+#'                    "lpjcell" for 67420 cells
+#'
+#' @return magpie object in cellular resolution from reference year onwards
+#'
+#' @author Kristine Karstens, Felicitas Beier
 #'
 #' @examples
-#' \dontrun{ calcOutput("CalibratedYields", aggregate = FALSE) }
+#' \dontrun{
+#' calcOutput("CalibratedYields", aggregate = FALSE)
+#' }
 #'
 #' @importFrom magpiesets findset
-#' @importFrom magclass getYears add_columns dimSums time_interpolate
-#' @importFrom madrat toolFillYears
-#' @importFrom mrcommons toolLPJmLVersion
+#' @importFrom magclass getYears getNames dimSums mbind
+#' @importFrom madrat calcOutput toolConditionalReplace
+#' @importFrom mrcommons toolCoord2Isocell toolIso2CellCountries
 
 calcCalibratedYields <- function(source = c(lpjml = "ggcmi_phase3_nchecks_9ca735cb", isimip = NULL),
-                                 climatetype = "GSWP3-W5E5:historical", refYear = "y1995") {
+                                 climatetype = "GSWP3-W5E5:historical", refYear = "y1995", cells = "magpiecell") {
 
     sizelimit <- getOption("magclass_sizeLimit")
     options(magclass_sizeLimit = 1e+12)
     on.exit(options(magclass_sizeLimit = sizelimit))
 
-    crops           <- setdiff(findset("kcr"), c("betr", "begr"))
+    # correct ref year format
+    if (!grepl("y", refYear)) {
+      refYear <- paste0("y", refYear)
+    }
 
-    yieldFAO_iso    <- calcOutput("FAOYield", cut = 0.98, aggregate = FALSE)[, refYear, crops]
-    yieldLPJmL_grid <- calcOutput("Yields", source = source, climatetype = climatetype,
-                                  aggregate = FALSE, supplementary = TRUE)
+    # extract crop list
+    crops          <- setdiff(findset("kcr"), c("betr", "begr"))
 
-    years           <- getYears(yieldLPJmL_grid$x, as.integer = TRUE)
-    years           <- years[years >= as.integer(gsub("y", "", refYear))]
-    otherYields     <- yieldLPJmL_grid$x[, years, setdiff(getNames(yieldLPJmL_grid$x, dim = 1), crops)]
-    weight          <- yieldLPJmL_grid$weight
-    yieldLPJmL_grid <- yieldLPJmL_grid$x[, years, crops]
+    # read FAO and LPJmL yields
+    yieldFAOiso    <- calcOutput("FAOYield", cut = 0.98, aggregate = FALSE)[, refYear, crops]
+    yieldLPJmLgrid <- calcOutput("Yields", source = source, climatetype = climatetype,
+                                  aggregate = FALSE, supplementary = TRUE, cells = cells)
 
-    areaMAG_grid    <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE, cellular = TRUE,
+    years          <- getYears(yieldLPJmLgrid$x, as.integer = TRUE)
+    years          <- years[years >= as.integer(gsub("y", "", refYear))]
+    weight         <- yieldLPJmLgrid$weight
+
+    otherYields    <- yieldLPJmLgrid$x[, years, setdiff(getNames(yieldLPJmLgrid$x, dim = 1), crops)]
+    yieldLPJmLgrid <- yieldLPJmLgrid$x[, years, crops]
+
+    # adjust cell naming
+    if (cells == "lpjcell") {
+      yieldLPJmLgrid <- toolCoord2Isocell(yieldLPJmLgrid, cells = cells)
+    }
+
+    # crop-specific cropland area
+    cropareaMAGgrid <- calcOutput("Croparea", sectoral = "kcr", physical = TRUE,
+                                  cellular = TRUE,  cells = cells,
                                   irrigation = TRUE, aggregate = FALSE)[, refYear, crops]
-    CountryToCell   <- toolGetMapping(type = "cell", name = "CountryToCellMapping.csv")
-    areaMAG_iso     <- toolAggregate(dimSums(areaMAG_grid, dim = 3.1), rel = CountryToCell,
-                                     from = "celliso", to = "iso", dim = 1)
-    cropMAG_grid    <- dimSums(areaMAG_grid, dim = 3.2)
 
-    yieldLPJmL_iso  <- toolAggregate(dimSums(yieldLPJmL_grid[, refYear, ] * areaMAG_grid, dim = 3.2),
-                                     rel = CountryToCell, from = "celliso", to = "iso", dim = 1) / areaMAG_iso
+    # total irrigated & rainfed cropland (for correction of 0 cropland areas)
+    proxyMAGgrid    <- dimSums(cropareaMAGgrid, dim = "MAG")
 
-    yieldLPJmL_iso[areaMAG_iso == 0] <- (toolAggregate(dimSums(yieldLPJmL_grid[, refYear, ] * cropMAG_grid, dim = 3.2),
-                                                       rel = CountryToCell, from = "celliso", to = "iso", dim = 1) /
-                                           dimSums(areaMAG_iso, dim = 3))[areaMAG_iso == 0]
+    # Aggregate to country values
+    # Crop-specific cropland area per country
+    cropareaMAGiso  <- dimSums(cropareaMAGgrid, dim = "cell")
 
-    yieldLPJmL_iso <- toolConditionalReplace(yieldLPJmL_iso, "is.na()", 0)
-    yieldLPJmL_iso <- toolIso2CellCountries(yieldLPJmL_iso)
-    yieldFAO_iso   <- toolIso2CellCountries(yieldFAO_iso)
+    # Averaged LPJmL yield per country (LPJmL production / area)
+    yieldLPJmLiso   <- dimSums(dimSums(yieldLPJmLgrid[, refYear, ] * cropareaMAGgrid,
+                                       dim = 3.2),
+                               dim = "cell") / cropareaMAGiso
 
-    out <- toolPatternScaling(yieldLPJmL_grid, yieldLPJmL_iso, yieldFAO_iso, ref_year = refYear)
+    # Correction where no historical crop-specific areas given
+    yieldLPJmLiso[cropareaMAGiso == 0] <- (dimSums(dimSums(yieldLPJmLgrid[, refYear, ] * proxyMAGgrid,
+                                                           dim = 3.2),
+                                                   dim = "cell") / dimSums(cropareaMAGiso,
+                                                                           dim = 3))[cropareaMAGiso == 0]
+
+    # Correction NAs
+    yieldLPJmLiso <- toolConditionalReplace(yieldLPJmLiso, "is.na()", 0)
+
+    # Harmonize countries
+    yieldLPJmLiso <- yieldLPJmLiso[intersect(getCells(yieldLPJmLiso), getCells(yieldFAOiso)), , ]
+    yieldFAOiso   <- yieldFAOiso[intersect(getCells(yieldLPJmLiso), getCells(yieldFAOiso)), , ]
+
+    # Yield calibration of LPJmL yields to FAO country yield levels
+    out <- toolPatternScaling(yieldLPJmLgrid, yieldLPJmLiso, yieldFAOiso, ref_year = refYear)
     out <- mbind(out, otherYields)
 
-  return(list(x            = out,
-              weight       = weight,
-              unit         = "t DM per ha physical area",
-              description  = "Calibrated crop yields by plant type and irrigation",
-              isocountries = FALSE)
-  )
+    return(list(x            = out,
+                weight       = weight,
+                unit         = "t DM per ha physical area",
+                description  = "Calibrated crop yields by plant type and irrigation",
+                isocountries = FALSE))
 }
