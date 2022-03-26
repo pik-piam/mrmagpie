4322b330fc4222adc5a268129acf6d816f6ecea5
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-03-26 13:17:13 +0100

    fixed warning in convertGPD

--- R/convertGPD.R
+++ R/convertGPD.R
@@ -4,20 +4,21 @@
 #' @return List of magpie objects with results on iso level, weight, unit and description.
 #' @author Florian Humpenoeder
 #' @examples
-#'
 #' \dontrun{
-#'   readSource("GPD", convert=TRUE)
+#' readSource("GPD", convert = TRUE)
 #' }
-#' @importFrom readxl read_xls
+#'
+convertGPD <- function(x) {
 
-convertGPD <- function(x){
-
-  #convert to Mha
-  x <- x/1000
-
-  getCells(x) <- toolCountry2isocode(getCells(x))   #replace country names with iso3 codes
-  x           <- toolCountryFill(x)                 #expand to all countries
-  x[is.na(x)] <- 0                                  #replace empty spots with zeros
-
+  # convert to Mha
+  x <- x / 1000
+  getCells(x) <- toolCountry2isocode(getCells(x))   # replace country names with iso3 codes
+  # remove values for Netherland Antilles if they are 0 as they otherwise trigger warnings
+  # but only remove if they are really 0 as otherwise a split and redistribution of the
+  # data to nowadays existing ISO countries is required.
+  if (all(x["ANT", , ] == 0)) {
+    x <- x["ANT", , , invert = TRUE]
+  }
+  x <- toolCountryFill(x, fill = 0)       # fill missing countries with 0
   return(x)
 }
--- R/readGPD.R
+++ R/readGPD.R
@@ -1,7 +1,8 @@
 #' @title readGPD
 #' @description read GPD
 #' Data from the Global Peatland Database provided by Alexandra Barthelmes.
-#' The original xls file has been clean-up manually (country names). Turkey had two identical entries in the original xls file.
+#' The original xls file has been clean-up manually (country names). Turkey had
+#' two identical entries in the original xls file.
 #' Sources:
 #' "Inventory Reports and National Communications UNFCC 2014",
 #' "soil and peatland science",
@@ -10,19 +11,17 @@
 #' @return List of magpie objects with results on cellular level, weight, unit and description.
 #' @author Florian Humpenoeder
 #' @examples
-#'
 #' \dontrun{
-#'   readSource("GPD", convert="onlycorrect")
+#' readSource("GPD", convert = "onlycorrect")
 #' }
 #' @importFrom readxl read_xls
 
+readGPD <- function() {
 
-readGPD <- function(){
-
-  #read-in xls file Global Peatland Database
+  # read-in xls file Global Peatland Database
   x <- read_xls("Global_GPD_15_AB_1811020.xls")
-  #convert to magclass object
-  x <- as.magpie(x[,1:3],spatial=1)
-  
+  # convert to magclass object
+  x <- as.magpie(x[, 1:3], spatial = 1)
+
   return(x)
 }
