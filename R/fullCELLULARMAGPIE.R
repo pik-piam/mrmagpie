bd0faa855a0c8deda67fe4114df79fbb0f7fe343
Vartika Singh <51452385+vartika271987@users.noreply.github.com>
2022-04-13 10:55:42 +0530

    type 4 upgrade

--- R/fullCELLULARMAGPIE.R
+++ R/fullCELLULARMAGPIE.R
@@ -106,12 +106,12 @@ fullCELLULARMAGPIE <- function(rev = 0.1, dev = "",
 
     calcOutput("Yields", aggregate = FALSE, source = c(lpjml = lpjml[["crop"]], isimip = isimip),
                climatetype = climatetype, round = 2, years = lpjYears, file = paste0("lpj_yields_0.5.mz"),
-               weighting = "crop+irrigSpecific", indiaYields = TRUE, scaleFactor = 0.9)
+               weighting = "crop+irrigSpecific", indiaYields = TRUE, scaleFactor = 0.5)
 
 
     calcOutput("Yields", aggregate = "cluster", source = c(lpjml = lpjml[["crop"]], isimip = isimip),
                climatetype = climatetype, round = 2, years = lpjYears, file = paste0("lpj_yields_", ctype, ".mz"),
-               weighting = "crop+irrigSpecific", indiaYields = TRUE, scaleFactor = 0.9)
+               weighting = "crop+irrigSpecific", indiaYields = TRUE, scaleFactor = 0.5)
 
   }  else {
 
