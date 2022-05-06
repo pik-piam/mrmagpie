8958de822b9c7ba3d0bc00d373b467921ca8d13a
David Chen <davidch@lt916.pik-potsdam.de>
2022-05-06 10:42:27 +0200

    bug fix calcPackagingMarketingCosts

--- R/calcPackagingMarketingCosts.R
+++ R/calcPackagingMarketingCosts.R
@@ -1,6 +1,6 @@
 #' @title calcPackagingMarketingCosts
 #' @description calculates per-ton marketing and packaging costs for food that leaves a cell
-#' Currnetly assume expert guess 50 USD / ton of packaging/marketing costs 
+#' Currnetly assume expert guess 50 USD / ton of packaging/marketing costs
 #' (100 USD/t in model, of which half is already in GTAP)
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
 #' @author David M Chen
@@ -8,13 +8,12 @@
 calcPackagingMarketingCosts <- function() {
   
   packaged <- calcOutput("NonLocalTransport", aggregate = FALSE)[,,"Exported"]
-  
+
   out <- 50 * packaged
-  
+
   return(list(x = out,
               weight = NULL,
               unit = "million USD05",
               description = "Packaging costs in USD per t dm by country and product",
-              isocountries = TRUE))
-  
-}
+              isocountries = FALSE))
+  }
