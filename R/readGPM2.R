90e48e7e734c542a6bd13c6abf392b63fd2f0698
florianh <humpenoeder@pik-potsdam.de>
2023-08-11 12:32:24 +0200

    bugfix

--- R/readGPM2.R
+++ R/readGPM2.R
@@ -13,14 +13,23 @@ readGPM2 <- function() {
                       todisk = TRUE, memfrac = 0.5)
   withr::defer(terra::terraOptions(tempdir = tempdir()))
 
-  # read-in file
-  r <- terra::rast("peatMAY22_1x1_mw_RUS30.tif")
+  # Note: terra::project(r, r05, method = "sum") is currently not working on the cluster.
+  # Therefore, the object r2 has been pre-calculated and stored as peatMAY22_05deg_mw_RUS30.tif
+  if (file.exists("peatMAY22_05deg_mw_RUS30.tif")) {
+    r2 <- terra::rast("peatMAY22_05deg_mw_RUS30.tif")
+  } else {
+    # read-in file
+    r <- terra::rast("peatMAY22_1x1_mw_RUS30.tif")
 
-  # 0.5 deg raster object
-  r05 <- terra::rast(res = 0.5)
+    # 0.5 deg raster object
+    r05 <- terra::rast(res = 0.5)
 
-  # project r to 0.5 deg raster
-  r2 <- terra::project(r, r05, method = "sum")
+    # project r to 0.5 deg raster
+    r2 <- terra::project(r, r05, method = "sum")
+
+    # save
+    terra::writeRaster(r2, "peatMAY22_05deg_mw_RUS30.tif", overwrite = TRUE)
+  }
 
   # get cell area
   a <- terra::cellSize(r2[[1]], unit = "ha", mask = TRUE) * 1e-6
