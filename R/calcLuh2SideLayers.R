e68751577e684829728b3b8e1cbcfd081b196dc2
Kristine Karstens <karstens@pik-potsdam.de>
2023-09-20 15:22:56 +0200

    bugfix sidelayer function to work with magpiecell

--- R/calcLuh2SideLayers.R
+++ R/calcLuh2SideLayers.R
@@ -19,24 +19,16 @@ calcLuh2SideLayers <- function(cells = "lpjcell") {
   x  <- readSource("BendingTheCurve", subtype = "luh2_side_layers", convert = "onlycorrect")
 
   if (cells == "magpiecell") {
-
-      out <- toolCoord2Isocell(x)
-
-  } else if (cells == "lpjcell") {
-
-      out <- x
-
-  } else {
-      stop("Please specify cells argument")
+    x <- toolCoord2Isocell(x, cells = cells)
   }
 
   weight <- calcOutput("LandArea", cells = cells, aggregate = FALSE)
 
-return(list(
-  x = x,
-  weight = weight,
-  unit = "boolean",
-  description = paste0("Data from LUH2 provided by David Leclere from IIASA, ",
-                       "Bending the curve on biodiversity loss"),
-  isocountries = FALSE))
+  return(list(
+    x = x,
+    weight = weight,
+    unit = "boolean",
+    description = paste0("Data from LUH2 provided by David Leclere from IIASA, ",
+                         "Bending the curve on biodiversity loss"),
+    isocountries = FALSE))
 }
