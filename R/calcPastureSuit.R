d31d60b4ceb996205ea1526314e7fb03b8eccfc0
mppalves <mppalves@gmail.com>
2023-12-04 21:26:30 +0100

    new past calc suit

--- R/calcPastureSuit.R
+++ R/calcPastureSuit.R
@@ -15,10 +15,10 @@
 #' }
 #' @importFrom raster area rasterFromXYZ
 
-calcPastureSuit <- function(climatetype = "MRI-ESM2-0:ssp126",
-                            lpjml       =  "LPJmL4_for_MAgPIE_44ac93de",
-                            cells       = "lpjcell") {
 
+calcPastureSuit <- function(climatetype = "MRI-ESM2-0:ssp126",
+                            lpjml = "LPJmL4_for_MAgPIE_44ac93de",
+                            cells = "lpjcell") {
   x <- toolSplitSubtype(climatetype, list(climatemodel = NULL, scenario = NULL))
 
   # Extract stage argument information
@@ -29,30 +29,53 @@ calcPastureSuit <- function(climatetype = "MRI-ESM2-0:ssp126",
   }
 
   # Drivers of managed pastures
-  population <- calcOutput("GridPop", subtype = "all", cellular = TRUE, FiveYear = TRUE, harmonize_until = 2015,
-                           aggregate = FALSE)[, , toupper(substring(x$scenario, first = 0, last = 4))]
+  population <- calcOutput("GridPop",
+    subtype = "all", cellular = TRUE, FiveYear = TRUE, harmonize_until = 2015,
+    aggregate = FALSE
+  )[, , toupper(substring(x$scenario, first = 0, last = 4))]
 
-  cellPrep   <- calcOutput("LPJmLClimateInput", climatetype  = climatetype,
-                           variable     = "precipitation:monthlySum",
-                           stage        = stage,
-                           lpjmlVersion = lpjml,
-                           aggregate    = FALSE)
+  cellPrep <- calcOutput("LPJmLClimateInput",
+    climatetype = climatetype,
+    variable = "precipitation:monthlySum",
+    stage = stage,
+    lpjmlVersion = lpjml,
+    aggregate = FALSE
+  )
 
 
-  cellPet    <- calcOutput(type = "LPJmL_new", climatetype = climatetype,
-                           subtype   = "mpet",
-                           stage     = stage,
-                           version   = lpjml,
-                           aggregate = FALSE)
+  cellPet <- calcOutput(
+    type = "LPJmL_new", climatetype = climatetype,
+    subtype = "mpet",
+    stage = stage,
+    version = lpjml,
+    aggregate = FALSE
+  )
 
-  yearsCellPet      <- intersect(getYears(cellPet), findset("time"))
-  yearsCellPrep     <- intersect(findset("time"), getYears(cellPrep))
-  years             <- intersect(yearsCellPet, yearsCellPrep)
-  cellPrep          <- dimSums(cellPrep[, years, ], dim = 3)
-  cellPet           <- dimSums(cellPet[, years, ], dim = 3)
+
+  yearsCellPet <- intersect(getYears(cellPet), findset("time"))
+  yearsCellPrep <- intersect(findset("time"), getYears(cellPrep))
+  years <- intersect(yearsCellPet, yearsCellPrep)
+  cellPrep <- dimSums(cellPrep[, years, ], dim = 3)
+  cellPet <- dimSums(cellPet[, years, ], dim = 3)
 
   # Cell area calculation
-  landArea <- calcOutput("LandArea", aggregate = FALSE) / 100 # transformed to km^2
+
+  landcoords <- cellPet[, 1, 1]
+  landcoords[] <- 1
+  landcoords <- as.RasterBrick(landcoords)
+
+  cellSize <- raster::area(landcoords)
+  cellSize <- cellSize * landcoords
+  cellSize <- as.magpie(cellSize)
+  landArea <- cellSize
+
+  cellorder <- population
+  dimnames(cellorder)$x.y.iso <- gsub("\\.\\w+$", "", dimnames(cellorder)$x.y.iso)
+  cellorder <- match(dimnames(cellorder)$x.y.iso, dimnames(landArea)$x.y)
+
+  # Reorder the dimensions of landArea to match Population
+  landArea <- landArea[cellorder, , ]
+  getCells(landArea) <- getCells(population)
 
   # population density
   popDensity <- (population * 1e6) / landArea # population density in number of people per km2
@@ -70,48 +93,28 @@ calcPastureSuit <- function(climatetype = "MRI-ESM2-0:ssp126",
   # pasture suitability check
   pastureSuit <- aridity
   popDensity <- popDensity[, getYears(pastureSuit), ]
+
+  cellorder <- match(dimnames(pastureSuit)$x.y.iso, dimnames(popDensity)$x.y.iso)
+  popDensity <- popDensity[cellorder]
   pastureSuit[popDensity < 5] <- 0 # 5 hab km2 population threshold for managed pastures. Same from HYDE 3.2.
 
   pastureSuitArea <- (pastureSuit * landArea * 100) / 1e6 # (from km2 (x100) to mha (/1e6))
-  pastureSuitArea <- collapseDim(pastureSuitArea)
+
+  # New part: Calculating changing year after year
+
+  #pastureSuitAreaChanges <- pastureSuitArea
+  #for (i in 1:(length(getYears(pastureSuitArea)) - 1)) {
+  #  pastureSuitAreaChanges[, i + 1, ] <- (pastureSuitArea[, i + 1, ] - pastureSuitArea[, i, ])
+  #}
+
+  #pastureSuitAreaChanges[, 1, ] <- 0
   pastureSuitArea <- toolHoldConstantBeyondEnd(pastureSuitArea)
 
-  # calibration to historical values
-  histPastr <- setNames(calcOutput("LanduseInitialisation", cellular = TRUE,
-                                   nclasses = "nine", aggregate = FALSE)[, , c("past")], "pastr")
-  pastAll <- intersect(getYears(histPastr), getYears(pastureSuitArea))
-
-  pastLy <- findset("past")
-  pastLy <- pastLy[length(pastLy)] # past last year
-  future <- setdiff(getYears(pastureSuitArea), pastAll)
-
-  pastureSuitAreaReg <- dimSums(pastureSuitArea, dim = c("x", "y"))
-  histPastrReg       <- dimSums(histPastr, dim = c("x", "y"))
-  calibReg <- histPastrReg[, pastLy, ] / pastureSuitAreaReg[, pastLy, ]
-  calibReg[is.infinite(calibReg)] <- 1
-  calibReg[is.nan(calibReg)]      <- 0
-  pastureSuitArea[, future, ] <- calibReg * pastureSuitArea[, future, ]
-
-  pastureSuitArea[is.infinite(pastureSuitArea) | is.nan(pastureSuitArea) | is.na(pastureSuitArea)] <- 0
-  pastureSuitArea[pastureSuitArea < 0] <- 0
-  pastureSuitArea[, pastAll, ] <- histPastr[, pastAll, ]
-
-  y <- intersect(pastAll, getYears(pastureSuitArea))
-  pastureSuitArea[, y, ] <- histPastr[, intersect(pastAll, getYears(pastureSuitArea)), ]
-  pastureSuitArea <- toolHoldConstant(pastureSuitArea, findset("time"))
-  pastureSuitArea <- collapseNames(pastureSuitArea)
-  pastureSuitArea[, pastAll, ] <- histPastr[, pastAll, ]
-  #maybe change to: pastureSuitArea[, pastAll, ] <- max(histPastr[, pastAll, ], pastureSuitArea[, pastAll, ]) #nolint
-  pastureSuitArea <- setNames(pastureSuitArea, "yields")
-
-  # Reduce number of grid cells to 59199
-  if (cells == "magpiecell") {
-    pastureSuitArea <- toolCoord2Isocell(pastureSuitArea, cells = cells)
-  }
-
-  return(list(x            = pastureSuitArea,
-              weight       = NULL,
-              unit         = "Mha",
-              description  = "Area suitable for pasture management in mha",
-              isocountries = FALSE))
+  return(list(
+    x = pastureSuitArea,
+    weight = NULL,
+    unit = "Mha",
+    description = "Area suitable for pasture management in mha",
+    isocountries = FALSE
+  ))
 }
