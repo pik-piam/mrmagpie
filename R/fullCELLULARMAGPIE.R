a8b6a9d9be0a1015cbef802dd4ec8f33d774f86d
Kristine Karstens <karstens@pik-potsdam.de>
2024-01-16 22:54:03 +0100

    add mrsoil to DESCRIPTION

--- R/fullCELLULARMAGPIE.R
+++ R/fullCELLULARMAGPIE.R
@@ -40,6 +40,7 @@
 #' @importFrom luplot plotregionscluster
 #' @importFrom ggplot2 ggsave
 #' @importFrom withr local_options
+#' @import mrsoil
 
 fullCELLULARMAGPIE <- function(rev = 0.1, dev = "",
                                ctype = "c200",
