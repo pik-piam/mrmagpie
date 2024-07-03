6474ce037ed64358bccf83295fdcc71f2ef6caae
florianh <humpenoeder@pik-potsdam.de>
2024-07-03 18:41:10 +0200

    added 500m peatland map

--- R/readGPM2.R
+++ R/readGPM2.R
@@ -1,5 +1,6 @@
 #' @title readGPM2
 #' @description read peatland area from GPM2
+#' @param type resolution ("1km" or "500m")
 #' @return List of magpie objects with results on cellular level, weight, unit and description.
 #' @author Florian Humpenoeder
 #' @examples
@@ -8,30 +9,54 @@
 #' }
 #' @importFrom magclass as.magpie
 #' @importFrom mstools toolGetMappingCoord2Country
-readGPM2 <- function() {
+readGPM2 <- function(type = "1km") {
   previousOptions <- terra::terraOptions(print = FALSE)
   terra::terraOptions(tempdir = withr::local_tempdir(tmpdir = getConfig("tmpfolder")),
                       todisk = FALSE, memfrac = 0.5)
   withr::defer(do.call(terra::terraOptions, previousOptions))
 
-  # read-in file
-  r <- terra::rast("peatMAY22_1x1_mw_RUS30.tif")
+  if (type == "1km") {
+    # read-in file
+    r <- terra::rast("peatMAY22_1x1_mw_RUS30.tif")
 
-  # choose only 1st layer
-  r1 <- terra::segregate(r, other = NA)
-  r1 <- r1[[1]]
+    # choose only 1st layer
+    r1 <- terra::segregate(r, other = NA)
+    r1 <- r1[[1]]
 
-  # project r to lon/lat
-  r2 <- terra::project(r1, "+proj=longlat +datum=WGS84", method = "near")
+    # project r to lon/lat
+    r2 <- terra::project(r1, "+proj=longlat +datum=WGS84", method = "near")
 
-  # get cell area
-  a <- terra::cellSize(r2, unit = "ha", mask = TRUE)
-  a <- a * 1e-6
+    # get cell area
+    a <- terra::cellSize(r2, unit = "ha", mask = TRUE)
+    a <- a * 1e-6
 
-  # project or aggregate to 0.5 degree
-  # use terra::aggregate because terra::project(a, terra::rast(res = 0.5), method = "sum") is not working
-  # on the cluster (method = "sum" is the problem)
-  r3 <- terra::aggregate(a, fact = 48, fun = "sum", na.rm = TRUE)
+    # project or aggregate to 0.5 degree
+    # use terra::aggregate because terra::project(a, terra::rast(res = 0.5), method = "sum") is not working
+    # on the cluster (method = "sum" is the problem)
+    r3 <- terra::aggregate(a, fact = 48, fun = "sum", na.rm = TRUE)
+
+  } else if (type == "500m") {
+    # read-in file
+    r <- terra::vrt(list.files("500m", full.names = TRUE, pattern = ".tif$"), "500m/GPM2.0_500m.vrt", overwrite = TRUE)
+    crs(r) <- "+proj=moll"
+
+    # choose only 1st layer
+    r1 <- terra::segregate(r, other = NA)
+    r1 <- r1[[1]]
+
+    # project r to lon/lat
+    r2 <- terra::project(r1, "+proj=longlat +datum=WGS84", method = "near")
+
+    # get cell area
+    a <- terra::cellSize(r2, unit = "ha", mask = TRUE)
+    a <- a * 1e-6
+
+    # project or aggregate to 0.5 degree
+    # use terra::aggregate because terra::project(a, terra::rast(res = 0.5), method = "sum") is not working
+    # on the cluster (method = "sum" is the problem)
+    r3 <- terra::aggregate(a, fact = 96, fun = "sum", na.rm = TRUE)
+
+  }
 
   # get spatial mapping
   map <- toolGetMappingCoord2Country(pretty = TRUE)
